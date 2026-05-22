3f34a87c783f1a8666a044d75ccb1684b9c7c31e
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2026-05-22 15:56:38 +0200

    Create unreal project

---
+++ Source/Raid2026/Raid2026.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Raid2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Raid2026, "Raid2026" );
