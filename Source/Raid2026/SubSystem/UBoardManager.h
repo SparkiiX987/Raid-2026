6e2645f7bc10ce20ec69005f176c5fb991e4d7b3
Jsp872 <celian.juan.conti47@gmail.com>
2026-06-01 16:06:29 +0200

    add folder subsystem and start board manager

---
+++ Source/Raid2026/SubSystem/UBoardManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UBoardManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAID2026_API UUBoardManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
