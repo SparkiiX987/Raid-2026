75a5c98b7e40b4db4674dd6b6635f300206ee755
Jsp872 <celian.juan.conti47@gmail.com>
2026-06-01 22:23:47 +0200

    Start the Board manager

--- Source/Raid2026/SubSystem/UBoardManager.cpp
+++ Source/Raid2026/SubSystem/UBoardManager.cpp
@@ -1,5 +1,115 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UBoardManager.h"
+#include "Raid2026/CoreLayer/FCell.h"
 
+bool UUBoardManager::IsValidCell(FIntPoint Pos) const
+{
+	return Pos.X >= 0 && Pos.X < GridWidth && Pos.Y >= 0 && Pos.Y < GridHeight;
+}
+
+bool UUBoardManager::IsCellOccupied(FIntPoint Pos) const
+{
+	return false;
+	//return IsValidCell(Pos) && Grid[Pos.X][Pos.Y].Occupant.IsValid();
+}
+
+FCell UUBoardManager::GetCell(FIntPoint Pos) const
+{
+	check(IsValidCell(Pos));
+	return Grid[Pos.X][Pos.Y];
+}
+
+FCell UUBoardManager::GetCell(int32 X, int32 Y) const
+{
+	check(IsValidCell(FIntPoint(X, Y)));
+	return Grid[X][Y];
+}
+
+// AShipActor* UUBoardManager::GetShipAt(FIntPoint Pos) const
+// {
+// 	return Grid[Pos.X][Pos.Y].Occupant;
+// }
+
+// AMothershipActor* UUBoardManager::GetMothershipAt(FIntPoint Pos) const
+// {
+// 	return Grid[Pos.X][Pos.Y].Occupant;
+// }
+
+bool UUBoardManager::IsMothershipCell(FIntPoint Pos, int32 ShooterPlayerID) const
+{
+	// if (Pos = GetMothershipAt(Pos).pos && GetMothershipAt(Pos).ID != ShooterPlayerID)
+	// {
+	// 	return true;
+	// }
+	return false;
+}
+
+TArray<FIntPoint> UUBoardManager::GetFreeSpawnCells(int32 PlayerID) const
+{
+	return TArray<FIntPoint>();
+}
+
+TArray<FReachableCell> UUBoardManager::GetReachableCells(AShipActor* Ship, int32 AvailableEssence) const
+{
+	TArray<FReachableCell> ReachableCells;
+	for (int32 X = 0; X < GridWidth; X++)
+	{
+		for (int32 Y = 0; Y < GridHeight; Y++)
+		{
+			// if (Grid[X][Y]
+			// }
+		}
+	}
+	return ReachableCells;
+}
+
+bool UUBoardManager::IsLineOfSight(FIntPoint From, FIntPoint To) const
+{
+	return false;
+}
+
+int32 UUBoardManager::CheckRefineries()
+{
+	return 0;
+}
+
+// bool UUBoardManager::MoveShipTo(AShipActor* Ship, FIntPoint TargetCell)
+// {
+// 	if (Grid[TargetCell.X][TargetCell.Y].IsEmpty())
+// 	{
+// 		SetOccupant(TargetCell, Ship);
+// 		return true;
+// 	}
+// 	return false;
+// }
+
+// void UUBoardManager::RemoveShipFromGrid(AShipActor* Ship)
+// {
+// 	ClearOccupant(Ship.pos);
+// }
+
+void UUBoardManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UUBoardManager::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+FCell& UUBoardManager::GetCellRef(FIntPoint Pos)
+{
+	check(IsValidCell(Pos));
+	return Grid[Pos.X][Pos.Y];
+}
+
+// void UUBoardManager::SetOccupant(FIntPoint Pos, AHordeActor* Actor)
+// {
+// 	Grid[Pos.X][Pos.Y].Occupant = Actor;
+// }
+
+void UUBoardManager::ClearOccupant(FIntPoint Pos)
+{
+	check(IsValidCell(Pos));
+	//Grid[Pos.X][Pos.Y].Occupant.Reset();
+}
--- Source/Raid2026/SubSystem/UBoardManager.h
+++ Source/Raid2026/SubSystem/UBoardManager.h
@@ -1,17 +1,87 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Raid2026/CoreLayer/FCell.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "UBoardManager.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class RAID2026_API UUBoardManager : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
+
+	static constexpr int32 GridWidth = 5;
+	static constexpr int32 GridHeight = 7;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnVictoryConditionMet, int32, WinnerPlayerID);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnVictoryConditionMet OnVictoryConditionMet;
+
+	//DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRefineryControlChanged, ARefineryActor*, Refinery, int32, NewOwnerID);
+
+	//UPROPERTY(BlueprintAssignable)
+	//FOnRefineryControlChanged OnRefineryControlChanged;
+
+	//UFUNCTION(BlueprintCallable)
+	//void InitializeBoard(const TArray<ARefineryActor*>& Refineries, AMothershipActor* MothershipP0,AMothershipActor* MothershipP1);
+
+	UFUNCTION(BlueprintPure)
+	bool IsValidCell(FIntPoint Pos) const;
+
+	UFUNCTION(BlueprintPure)
+	bool IsCellOccupied(FIntPoint Pos) const;
+
+	UFUNCTION(BlueprintPure)
+	FCell GetCell(FIntPoint Pos) const;
+
+	UFUNCTION(BlueprintPure)
+	FCell GetCell(int32 X, int32 Y) const;
+
+	//UFUNCTION(BlueprintPure)
+	//AShipActor* GetShipAt(FIntPoint Pos) const;
+
+	// UFUNCTION(BlueprintPure)
+	// AMothershipActor* GetMothershipAt(FIntPoint Pos) const;
+
+	UFUNCTION(BlueprintPure)
+	bool IsMothershipCell(FIntPoint Pos, int32 ShooterPlayerID) const;
+
+	UFUNCTION(BlueprintPure)
+	TArray<FIntPoint> GetFreeSpawnCells(int32 PlayerID) const;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FReachableCell> GetReachableCells(AShipActor* Ship, int32 AvailableEssence) const;
+
+	UFUNCTION(BlueprintPure)
+	bool IsLineOfSight(FIntPoint From, FIntPoint To) const;
+
+	UFUNCTION(BlueprintCallable)
+	int32 CheckRefineries();
+
+	//UFUNCTION(BlueprintCallable)
+	//bool MoveShipTo(AShipActor* Ship, FIntPoint TargetCell);
+
+	//UFUNCTION(BlueprintCallable)
+	//void RemoveShipFromGrid(AShipActor* Ship);
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void Deinitialize() override;
+
+	FCell Grid[GridWidth][GridHeight];
+
+	//TArray<ARefineryActor*> RefineryActors;
+
+	//TMap<int32, AMothershipActor*> Motherships;
+
+	FCell& GetCellRef(FIntPoint Pos);
+
+	//void SetOccupant(FIntPoint Pos, AHordeActor* Actor);
+
+	void ClearOccupant(FIntPoint Pos);
+
+	static const TArray<FIntPoint> OrthoDirections;
 };
+
+
