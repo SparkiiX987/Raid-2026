3abbadd5df3d7dd688b08df1e215712fe3b03552
Jsp872 <celian.juan.conti47@gmail.com>
2026-06-02 15:58:06 +0200

    Finish for now Board Manager

--- Source/Raid2026/SubSystem/UBoardManager.h
+++ Source/Raid2026/SubSystem/UBoardManager.h
@@ -9,7 +9,8 @@ UCLASS()
 class RAID2026_API UUBoardManager : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-
+	
+public:
 	static constexpr int32 GridWidth = 5;
 	static constexpr int32 GridHeight = 7;
 
@@ -18,68 +19,71 @@ class RAID2026_API UUBoardManager : public UGameInstanceSubsystem
 	UPROPERTY(BlueprintAssignable)
 	FOnVictoryConditionMet OnVictoryConditionMet;
 
-	//DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRefineryControlChanged, ARefineryActor*, Refinery, int32, NewOwnerID);
+	// DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRefineryControlChanged, ARefineryActor*, Refinery, int32, NewOwnerID);
 
-	//UPROPERTY(BlueprintAssignable)
-	//FOnRefineryControlChanged OnRefineryControlChanged;
+	// UPROPERTY(BlueprintAssignable)
+	// FOnRefineryControlChanged OnRefineryControlChanged;
 
-	//UFUNCTION(BlueprintCallable)
-	//void InitializeBoard(const TArray<ARefineryActor*>& Refineries, AMothershipActor* MothershipP0,AMothershipActor* MothershipP1);
+	UFUNCTION(BlueprintCallable)
+	void InitializeBoard(const TArray<AActor*>& Refineries, AActor* MothershipP0,AActor* MothershipP1);
 
 	UFUNCTION(BlueprintPure)
 	bool IsValidCell(FIntPoint Pos) const;
 
-	UFUNCTION(BlueprintPure)
-	bool IsCellOccupied(FIntPoint Pos) const;
+	// UFUNCTION(BlueprintPure)
+	// bool IsCellOccupied(FIntPoint Pos) const;
 
 	UFUNCTION(BlueprintPure)
 	FCell GetCell(FIntPoint Pos) const;
 
 	UFUNCTION(BlueprintPure)
-	FCell GetCell(int32 X, int32 Y) const;
-
-	//UFUNCTION(BlueprintPure)
-	//AShipActor* GetShipAt(FIntPoint Pos) const;
+	FCell GetCells(int32 X, int32 Y) const;
 
 	// UFUNCTION(BlueprintPure)
-	// AMothershipActor* GetMothershipAt(FIntPoint Pos) const;
+	// AShipActor* GetShipAt(FIntPoint Pos) const;
+	//
+	//  UFUNCTION(BlueprintPure)
+	//  AMothershipActor* GetMothershipAt(FIntPoint Pos) const;
 
-	UFUNCTION(BlueprintPure)
-	bool IsMothershipCell(FIntPoint Pos, int32 ShooterPlayerID) const;
+	// UFUNCTION(BlueprintPure)
+	// bool IsMothershipCell(FIntPoint Pos, int32 ShooterPlayerID) const;
 
-	UFUNCTION(BlueprintPure)
-	TArray<FIntPoint> GetFreeSpawnCells(int32 PlayerID) const;
+	// UFUNCTION(BlueprintPure)
+	// TArray<FIntPoint> GetFreeSpawnCells(int32 PlayerID) const;
 
-	UFUNCTION(BlueprintCallable)
-	TArray<FReachableCell> GetReachableCells(AShipActor* Ship, int32 AvailableEssence) const;
+	// UFUNCTION(BlueprintCallable)
+	// TArray<FReachableCell> GetReachableCells(AShipActor* Ship, int32 AvailableEssence) const;
+	//
+	// UFUNCTION(BlueprintPure)
+	// bool IsLineOfSight(FIntPoint From, FIntPoint To) const;
 
-	UFUNCTION(BlueprintPure)
-	bool IsLineOfSight(FIntPoint From, FIntPoint To) const;
+	// UFUNCTION(BlueprintCallable)
+	// int32 CheckRefineries(int32 playerID);
 
-	UFUNCTION(BlueprintCallable)
-	int32 CheckRefineries();
-
-	//UFUNCTION(BlueprintCallable)
-	//bool MoveShipTo(AShipActor* Ship, FIntPoint TargetCell);
-
-	//UFUNCTION(BlueprintCallable)
-	//void RemoveShipFromGrid(AShipActor* Ship);
+	// UFUNCTION(BlueprintCallable)
+	// bool MoveShipTo(AShipActor* Ship, FIntPoint TargetCell,int32 playerID);
+	//
+	// UFUNCTION(BlueprintCallable)
+	// bool PlaceShip(AShipActor* Ship, FIntPoint target, int32 PlayerID);
+	//
+	// UFUNCTION(BlueprintCallable)
+	// void RemoveShipFromGrid(AShipActor* Ship);
 
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-
+	
 	virtual void Deinitialize() override;
 
 	FCell Grid[GridWidth][GridHeight];
 
-	//TArray<ARefineryActor*> RefineryActors;
-
-	//TMap<int32, AMothershipActor*> Motherships;
+	// TArray<ARefineryActor*> RefineryActors;
+	//
+	// TMap<int32, AMothershipActor*> Motherships;
 
 	FCell& GetCellRef(FIntPoint Pos);
 
-	//void SetOccupant(FIntPoint Pos, AHordeActor* Actor);
+	// void SetOccupant(FIntPoint Pos, ABoardActor* Actor);
 
-	void ClearOccupant(FIntPoint Pos);
+	//void ClearOccupant(FIntPoint Pos);
 
 	static const TArray<FIntPoint> OrthoDirections;
 };
