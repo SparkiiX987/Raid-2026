2e48c98dc0839eec7111374ced5dbedfd0442803
Jsp872 <celian.juan.conti47@gmail.com>
2026-06-05 13:47:34 +0200

    Begin the multiplayer of playercontroller

--- Source/Raid2026/PlayerController/BoardPlayerController.cpp
+++ Source/Raid2026/PlayerController/BoardPlayerController.cpp
@@ -1,27 +1,136 @@
 #include "BoardPlayerController.h"
 
+void ABoardPlayerController::OnRep_PlayerID()
+{
+	
+}
+
+void ABoardPlayerController::SetPlayerID(int32 ID)
+{
+	PlayerID = ID;
+}
+
 void ABoardPlayerController::ClickOnShip(AAShip* selectedShip)
 {
-	if (UUTurnManager->GetCurrentPlayer() != selectedShip->GetOwnerID())
-	{
-		return;
-	}
-	if (selectedShip == SelectedShip)
-	{
-		SelectedShip = nullptr;
-		return;
-	}
-
-	SelectedShip = selectedShip;
+	// if (UUTurnManager->GetCurrentPlayer() != selectedShip->GetOwnerID())
+	// {
+	// 	return;
+	// }
+	// if (selectedShip == SelectedShip)
+	// {
+	// 	SelectedShip = nullptr;
+	// 	return;
+	// }
+	//
+	// SelectedShip = selectedShip;
 }
 
 void ABoardPlayerController::ClickOnCell(AABoardCell* selectCell)
 {
 
-	if (!IsValid(SelectedShip) && !BoardManager->IsCellOccupied(selectCell->cellData.Pos))
-	{
-		BoardVisualiser->SpawnShip(BoardVisualiser->ShipClass, selectCell->cellData.Pos, CardData, UUTurnManager->GetCurrentPlayer());
-		return;
-	}
-	BoardManager->MoveShipTo(SelectedShip, selectCell->cellData.Pos, UUTurnManager->GetCurrentPlayer());
+	// if (!IsValid(SelectedShip) && !BoardManager->IsCellOccupied(selectCell->cellData.Pos))
+	// {
+	// 	BoardVisualiser->SpawnShip(BoardVisualiser->ShipClass, selectCell->cellData.Pos, CardData, UUTurnManager->GetCurrentPlayer());
+	// 	return;
+	// }
+	// BoardManager->MoveShipTo(SelectedShip, selectCell->cellData.Pos, UUTurnManager->GetCurrentPlayer());
+}
+
+void ABoardPlayerController::RequestEndTurn()
+{
+}
+
+void ABoardPlayerController::RequestPlayCard(UUCardData* Card, FIntPoint TargetCell)
+{
+}
+
+void ABoardPlayerController::ServerMoveShip_Implementation(AAShip* Ship, FIntPoint TargetCell)
+{
+}
+
+bool ABoardPlayerController::ServerMoveShip_Validate(AAShip* Ship, FIntPoint TargetCell)
+{
+	return true;
+}
+
+void ABoardPlayerController::ServerFireAt_Implementation(AAShip* Shooter, FIntPoint TargetCell)
+{
+}
+
+bool ABoardPlayerController::ServerFireAt_Validate(AAShip* Shooter, FIntPoint TargetCell)
+{
+	return true;
+}
+
+void ABoardPlayerController::ServerPlayCard_Implementation(UUCardData* Card, FIntPoint TargetCell)
+{
+}
+
+bool ABoardPlayerController::ServerPlayCard_Validate(UUCardData* Card, FIntPoint TargetCell)
+{
+	return true;
+}
+
+void ABoardPlayerController::ServerSpawnShip_Implementation(AAShip* Ship, FIntPoint TargetCell)
+{
+}
+
+bool ABoardPlayerController::ServerSpawnShip_Validate(AAShip* Ship, FIntPoint TargetCell)
+{
+	return true;
+}
+
+void ABoardPlayerController::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
+
+void ABoardPlayerController::HandleShipSelected(AAShip* Ship)
+{
+}
+
+void ABoardPlayerController::HandleCellTargeted(AABoardCell* Cell)
+{
+}
+
+void ABoardPlayerController::ClearSelection()
+{
+	SelectedShip = nullptr;
+}
+
+void ABoardPlayerController::ClientOnVictory_Implementation(int32 WinnerID)
+{
+}
+
+void ABoardPlayerController::ClientOnEssenceChanged_Implementation(int32 NewEssence, int32 MaxEssence)
+{
+}
+
+void ABoardPlayerController::ClientOnShipDestroyed_Implementation(AAShip* Ship)
+{
+}
+
+void ABoardPlayerController::ClientOnShipMoved_Implementation(AAShip* Ship, FIntPoint NewCell)
+{
+}
+
+void ABoardPlayerController::ClientOnActionRejected_Implementation(const FString& Reason)
+{
+}
+
+void ABoardPlayerController::ClientUpdateHand_Implementation(const TArray<UUCardData*>& NewHand)
+{
+}
+
+void ABoardPlayerController::ClientOnTurnEnded_Implementation()
+{
+}
+
+void ABoardPlayerController::ClientOnTurnStarted_Implementation(int32 ActivePlayerID)
+{
+	SetPlayerID(ActivePlayerID);
+}
+
+void ABoardPlayerController::ServerEndTurn_Implementation()
+{
 }
--- Source/Raid2026/PlayerController/BoardPlayerController.h
+++ Source/Raid2026/PlayerController/BoardPlayerController.h
@@ -14,25 +14,101 @@ class RAID2026_API ABoardPlayerController : public APlayerController
 
 public:
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UUBoardManager* BoardManager;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_PlayerID)
+	int32 PlayerID = -1;
+
+	UFUNCTION()
+	void OnRep_PlayerID();
+
+	void SetPlayerID(int32 ID);
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	AABoardVisualiser* BoardVisualiser;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UUTurnManager* UUTurnManager;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(BlueprintReadOnly)
 	AAShip* SelectedShip;
 
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsMyTurn;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UUCardData* CardData;
+	TObjectPtr<AABoardVisualiser> BoardVisualiser;
 
 	UFUNCTION(BlueprintCallable)
 	void ClickOnShip(AAShip* selectShip);
 	
 	UFUNCTION(BlueprintCallable)
 	void ClickOnCell(AABoardCell* selectedCell);
+
+	UFUNCTION(BlueprintCallable)
+	void RequestEndTurn();
+
+	UFUNCTION(BlueprintCallable)
+	void RequestPlayCard(UUCardData* Card, FIntPoint TargetCell);
 	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerMoveShip(AAShip* Ship, FIntPoint TargetCell);
+	bool ServerMoveShip_Validate(AAShip* Ship, FIntPoint TargetCell);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerFireAt(AAShip* Shooter, FIntPoint TargetCell);
+	bool ServerFireAt_Validate(AAShip* Shooter, FIntPoint TargetCell);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerPlayCard(UUCardData* Card, FIntPoint TargetCell);
+	bool ServerPlayCard_Validate(UUCardData* Card, FIntPoint TargetCell);
+
+	UFUNCTION(Server, Reliable)
+	void ServerEndTurn();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSpawnShip(AAShip* Ship, FIntPoint TargetCell);
+	bool ServerSpawnShip_Validate(AAShip* Ship, FIntPoint TargetCell);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnTurnStarted(int32 ActivePlayerID);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnTurnEnded();
+
+	UFUNCTION(Client, Reliable)
+	void ClientUpdateHand(const TArray<UUCardData*>& NewHand);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnActionRejected(const FString& Reason);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnShipMoved(AAShip* Ship, FIntPoint NewCell);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnShipDestroyed(AAShip* Ship);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnEssenceChanged(int32 NewEssence, int32 MaxEssence);
+
+	UFUNCTION(Client, Reliable)
+	void ClientOnVictory(int32 WinnerID);
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	bool bWaitingForCellTarget = false;
+	//EActionIntent PendingIntent = EActionIntent::None;
+
+	void HandleShipSelected(AAShip* Ship);
+	void HandleCellTargeted(AABoardCell* Cell);
+	void ClearSelection();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTurnStartedBP(int32 ActivePlayerID, bool bIsLocalPlayerTurn);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnHandUpdatedBP(const TArray<UUCardData*>& NewHand);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnActionRejectedBP(const FString& Reason);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnEssenceChangedBP(int32 NewEssence, int32 MaxEssence);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnVictoryBP(int32 WinnerID);
 };
+
+
